63f62539cfb645b05606ca42dbd3056b90444f37
jlfoose@hotmail.com <jlfoose@hotmail.com>
2025-09-02 21:07:34 +0200

    Added calculate_hypotenuse function for assignment 3

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-
-// This is where you add the function calculate_hypotenuse
+float calculate_hypotenuse(float a, float b) {
+    return sqrt(pow(a, 2) + pow(b, 2));
+}
 
 int main() {
     float a = 3.0;
